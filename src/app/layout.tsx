b8f20b17754964ff7631962eee92ab317bb655d0
Tiehisung <tiehisungai@gmail.com>
2023-07-31 03:26:13 +0000

    metadata removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 import "./globals.css";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
 import AuthProvider from "@/context/auth-provider";
 
 // const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Car hub",
-  description: "Discover the best cars in the world",
-};
+// export const metadata: Metadata = {
+//   title: "Car hub",
+//   description: "Discover the best cars in the world",
+// };
 interface Props {
   children: React.ReactNode;
 }
